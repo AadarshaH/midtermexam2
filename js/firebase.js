71e5aa0d17ca79679b12a807e735864bacfd7d59
Aadarsha Humagain <humagainaadarsha123@gmail.com>
2023-12-01 16:55:25 -0800

    added the javascripts

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCefU3YrxRaMEwOUPiVTkomZPxqNOBSqRA",
+    authDomain: "firauthlog.firebaseapp.com",
+    projectId: "firauthlog",
+    storageBucket: "firauthlog.appspot.com",
+    messagingSenderId: "926267407342",
+    appId: "1:926267407342:web:1d7dcdc962f8c71ab02cff",
+    measurementId: "G-HFWG41Y0G4"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
